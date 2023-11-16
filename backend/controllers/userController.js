b746ca18b8ce25fcdd4ae992043f0bbbcf928a82
Abdurrehman_Khan <abdurrehmanbinfaheem@gmail.com>
2023-11-16 23:46:48 +0500

    Initialize frontend

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -4,9 +4,9 @@ const Role = require("../models/role");
 async function createUser(req, res) {
     try {
         const {role, ...userData} = req.body;
-        const roleFound = await Role.findOne({roleName: role})
+        const roleFound = await Role.findOne({roleName: role});
         const userCompleteData = {...userData, role: roleFound};
-        const user = User(userCompleteData);
+        const user = await User(userCompleteData);
         user.save();
         res.status(201).json(user)
     } catch (err) {
@@ -17,7 +17,12 @@ async function createUser(req, res) {
 async function updateUser(req, res) {
     try {
         const {id} = req.params;
-        const updatedUser = await User.findByIdAndUpdate(id,req.body,{ new: true });
+
+        const {role, ...userData} = req.body;
+        const roleFound = await Role.findOne({roleName: role});
+        const userCompleteData = {...userData, role: roleFound};
+        
+        const updatedUser = await User.findByIdAndUpdate(id,userCompleteData,{ new: true });
         res.json(updatedUser)
     } catch (err) {
         res.status(500).json({ error : err.message })
@@ -36,7 +41,7 @@ async function getAllUsers(req, res) {
 async function deleteUser(req, res) {
     try {
         const {id} = req.params;
-        const deleteUser = await User.findByIdAndRemove(id);
+        const deleteUser = await User.findByIdAndDelete(id);
         res.sendStatus(204);
     } catch (err) {
         res.status(500).json({ error : err.message })
---
+++ frontend/src/components/App.js
@@ -0,0 +1,12 @@
+import '../App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Matcher App</h1>
+      <p>*************************</p>
+    </div>
+  );
+}
+
+export default App;
